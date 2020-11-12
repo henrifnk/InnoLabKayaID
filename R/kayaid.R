faa56e550cda144a9597921b71bfe429cf3905e3
HenriLMU <50552831+HenriLMU@users.noreply.github.com>
2020-11-12 09:54:09 +0100

    first draft kaya_id

---
+++ R/kayaid.R
@@ -0,0 +1,17 @@
+#' The Kaya Identity is an equation that expresses yearly CO2 emissions as a product of four factors
+#'
+#' The Kaya Identity can be computed as: co2 = pop  gdp  enInt  carbInt. You can see by the units that they automatically resolve to million tonnes of CO2.
+#'
+#' @param pop Population size (in millions)
+#' @param gdp GDP per capita (in 1000$/person)
+#' @param enINT Energy Intensity (in Gigajoule/$1000GDP). Energy Intensity is the energy needed to produce a certain amount of economic value.
+#' @param carbInt Carbon Intensity (in tonnes CO2/Gigajoule). Carbon Intensity is the CO2 emitted for produced energy. This number depends on the energy mix used (coal, solar, . . . ).
+#' @return million tonnes of CO2 emissions
+#' @example
+#' kaya_identity(82.4, 44,5,0.05)
+#' @export
+kaya_identity <- function(pop, gdp, enInt, carbInt) {
+
+  pop * gdp * enInt * carbInt
+
+}
